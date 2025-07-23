f9f05b29d110acfff783903bb0e2548eb9143ce8
James Ball <james.al.ball@gmail.com>
2025-07-22 20:32:16 -0400

    initial commit

---
+++ src/main/java/com/gooderreads/gooder_reads/GooderReadsApplication.java
@@ -0,0 +1,13 @@
+package com.gooderreads.gooder_reads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooderReadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooderReadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gooderreads/gooder_reads/GooderReadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gooderreads.gooder_reads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooderReadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
